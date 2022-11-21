b5f613a3ae5ac4ac8a668f54c0d6dc6ee55f5d6d
Pronyushkin_qa <kollaydereva@gmail.com>
2022-11-22 00:22:48 +0300

    1

---
+++ pro/sky/course/vork/CourseVorkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.course.vork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseVorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseVorkApplication.class, args);
+	}
+
+}
